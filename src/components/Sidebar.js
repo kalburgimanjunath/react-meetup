40df75f4d5edd4ce6199678f8a5ef52abd7ea741
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-08-21 13:03:00 +0530

    2 files modified

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 export default function Sidebar() {
+  const [value, onChange] = useState(new Date());
   return (
     <div className="page sidebar">
       <h3>Events from your groups</h3>
-      <div>Page Calender</div>
+      <div>
+        <Calendar onChange={onChange} value={value} />
+      </div>
     </div>
   );
 }
